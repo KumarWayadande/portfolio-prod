4851d280e49c2c631cfa75bf6aa70e1460094df9
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-24 23:36:13 +0530

    Single Article - react-router loader removed

--- src/components/article-components/single-article/TempSingleArticle.jsx
+++ src/components/article-components/single-article/TempSingleArticle.jsx
@@ -1,5 +1,5 @@
 // This Component is being used for displaying demo article
-// This Component is being used for displaying demo
+// This Component is being used for displaying demo a
 // import axios from "axios";
 // import { useLoaderData } from "react-router-dom";
 import ArticleBackButton from "./ArticleBackBtn";
