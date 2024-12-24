041a6a00708a1aa0a4129bb2ca27a3063cd35d7f
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-24 23:31:05 +0530

    Single Article Added - 1

--- src/components/article-components/single-article/TempSingleArticle.jsx
+++ src/components/article-components/single-article/TempSingleArticle.jsx
@@ -1,5 +1,5 @@
 // This Component is being used for displaying demo article
-// This C
+// This Co
 // import axios from "axios";
 // import { useLoaderData } from "react-router-dom";
 import ArticleBackButton from "./ArticleBackBtn";
