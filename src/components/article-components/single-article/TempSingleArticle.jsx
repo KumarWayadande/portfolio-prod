a82650bacb9af6bf53adde59eb8891dc94d33410
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-24 23:31:41 +0530

    Single Article - axios removed

--- src/components/article-components/single-article/TempSingleArticle.jsx
+++ src/components/article-components/single-article/TempSingleArticle.jsx
@@ -1,5 +1,5 @@
 // This Component is being used for displaying demo article
-// This Compone
+// This Componen
 // import axios from "axios";
 // import { useLoaderData } from "react-router-dom";
 import ArticleBackButton from "./ArticleBackBtn";
