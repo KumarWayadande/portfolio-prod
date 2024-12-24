8fda162af94f6330714c5b71af248c1c1035e09a
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-24 23:34:54 +0530

    Single Article - Code Editor Added with No specific language support

--- src/components/article-components/single-article/TempSingleArticle.jsx
+++ src/components/article-components/single-article/TempSingleArticle.jsx
@@ -1,5 +1,5 @@
 // This Component is being used for displaying demo article
-// This Component is being used for di
+// This Component is being used for dis
 // import axios from "axios";
 // import { useLoaderData } from "react-router-dom";
 import ArticleBackButton from "./ArticleBackBtn";
