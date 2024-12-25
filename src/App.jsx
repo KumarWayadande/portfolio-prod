9ab20ab6ac375d1a04cd929dc2db2d74c05b8004
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-25 19:49:01 +0530

    Scroll restoration done

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,10 @@ import { RouterProvider } from "react-router-dom";
 import router from "./components/pages/routes";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />;
+      {/* <ScrollRestoration /> */}
+    </>
+  );
 }
--- src/components/article-components/single-article/ArticleBackBtn.jsx
+++ src/components/article-components/single-article/ArticleBackBtn.jsx
@@ -1,10 +1,11 @@
 import { TfiArrowLeft } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 
-export default function ArticleBackButton() {
+export default function ArticleBackButton(prop) {
   return (
     <Link
       to={-1}
+      preventScrollReset={true}
       // onClick={(e) => e.preventDefault()}
       className="border dark:bg-[#252529] dark:border-gray-700 dark:hover:border-gray-600 border-gray-200 p-3 rounded-full shadow-md mb-10"
     >
