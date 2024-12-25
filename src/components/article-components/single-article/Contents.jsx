75a149a999c5070a629064192b310a5d64f92d7a
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-25 15:08:29 +0530

    Styling for code block added

--- src/components/article-components/single-article/Contents.jsx
+++ src/components/article-components/single-article/Contents.jsx
@@ -1,7 +1,7 @@
 // Component function for displaying paragraph contents in an article
 export default function Contents(prop) {
   const linkClasses =
-    "prose-a:text-[#14b8a6] prose-a:font-semibold prose-a:cursor-pointer prose-a:underline-none hover:prose-a:underline text-base my-5 leading-7 text-stone-600 dark:text-stone-400 prose-h1:font-bold dark:prose-h1:text-gray-100 prose-h1:py-4 prose-h1:text-4xl md:prose-h1:text-5xl dark:prose-h1:text-gray-100 prose-h2:font-medium md:prose-h2:text-3xl prose-h2:mt-10 prose-h2:mb-3 prose-h2:text-2xl dark:prose-h2:text-gray-100 prose-img:w-[100%] prose-img:rounded-2xl prose-img:my-10";
+    "prose-a:text-[#14b8a6] prose-a:font-semibold prose-a:cursor-pointer prose-a:underline-none hover:prose-a:underline text-base my-5 leading-7 text-stone-600 dark:text-stone-400 prose-h1:font-bold dark:prose-h1:text-gray-100 prose-h1:py-4 prose-h1:text-4xl md:prose-h1:text-5xl dark:prose-h1:text-gray-100 prose-h2:font-medium md:prose-h2:text-3xl prose-h2:mt-10 prose-h2:mb-3 prose-h2:text-2xl dark:prose-h2:text-gray-100 prose-img:w-[100%] prose-img:rounded-2xl prose-img:my-10 prose-code:block prose-code:bg-gray-100 prose-code:p-2 prose-code:rounded";
   return <p className={linkClasses}>{prop.children}</p>;
 }
 
