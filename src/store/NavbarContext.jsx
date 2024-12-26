cb72070332f76807b21568d930e21525b266a597
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-26 23:22:00 +0530

    navbar speaking component removed

--- src/store/NavbarContext.jsx
+++ src/store/NavbarContext.jsx
@@ -2,7 +2,7 @@
 
 
 // This file contains all the functions and states used for navbar and its buttons
-// This file contains all the fun 
+// This file contains all the func 
 
 import { createContext, useState } from "react";
 import LandingPage from "../components/utility components/LandingPage";
