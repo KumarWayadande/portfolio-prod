254091e2e82e9a789f3a9ef238d43e7a2cd7bf23
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-26 23:23:39 +0530

    social media icons links added

--- src/store/NavbarContext.jsx
+++ src/store/NavbarContext.jsx
@@ -2,7 +2,7 @@
 
 
 // This file contains all the functions and states used for navbar and its buttons
-// This file contains all the functions and states 
+// This file contains all the functions and states u 
 
 import { createContext, useState } from "react";
 import LandingPage from "../components/utility components/LandingPage";
