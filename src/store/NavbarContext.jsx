6895a104d2e995c0da214f7c32c6440ce295716a
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-26 23:32:14 +0530

    pdf of resume added

--- src/store/NavbarContext.jsx
+++ src/store/NavbarContext.jsx
@@ -3,7 +3,7 @@
 
 // This file contains all the functions and states used for navbar and its buttons
 // This file contains all the functions and states used for navbar and its buttons 
-// This file contain
+// This file contains a
 
 import { createContext, useState } from "react";
 import LandingPage from "../components/utility components/LandingPage";
