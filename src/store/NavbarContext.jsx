9b35306195c1511f8e3181131304ec69780e5c74
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-26 23:25:31 +0530

    newsletter subscribe option added

--- src/store/NavbarContext.jsx
+++ src/store/NavbarContext.jsx
@@ -2,7 +2,7 @@
 
 
 // This file contains all the functions and states used for navbar and its buttons
-// This file contains all the functions and states used for navbar and its but 
+// This file contains all the functions and states used for navbar and its butt 
 
 import { createContext, useState } from "react";
 import LandingPage from "../components/utility components/LandingPage";
