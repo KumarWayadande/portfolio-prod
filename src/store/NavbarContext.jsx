838da1a5f61e862c0479455a97ffb2cc82b22581
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-26 23:22:57 +0530

    Landing Page - content changed

--- src/store/NavbarContext.jsx
+++ src/store/NavbarContext.jsx
@@ -2,7 +2,7 @@
 
 
 // This file contains all the functions and states used for navbar and its buttons
-// This file contains all the functions an 
+// This file contains all the functions and 
 
 import { createContext, useState } from "react";
 import LandingPage from "../components/utility components/LandingPage";
