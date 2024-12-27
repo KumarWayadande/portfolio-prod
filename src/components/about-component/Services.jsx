bd2fce95c5f18c3d163b24c781a80e403ead3bef
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-28 00:29:07 +0530

    express logo added

--- src/components/about-component/Services.jsx
+++ src/components/about-component/Services.jsx
@@ -1,8 +1,8 @@
-// import html from "../../assets/html.jpg";
-// import css from "../../assets/css.jpg";
-// import js from "../../assets/js.jpg";
+import html from "../../assets/html.jpg";
+import css from "../../assets/css.jpg";
+import js from "../../assets/js.jpg";
 
-import express from "../../assets/express.png";
+// import express from "../../assets/express.png";
 import mongodb from "../../assets/mongodb.png";
 import react from "../../assets/react.png";
 import node from "../../assets/node.png";
