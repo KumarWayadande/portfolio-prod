93664e1af1fc6902c05bdf68ea34224c9cac8ca2
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-30 20:32:37 +0530

    tailwind changed

--- src/components/mobile-navbar/NavbarButtons.jsx
+++ src/components/mobile-navbar/NavbarButtons.jsx
@@ -18,7 +18,7 @@ export default function NavbarButtons() {
           onClick={handleModalClick}
           className="dark:hover:border-gray-500 dark:shadow-none dark:border-gray-600 dark:border-[1px] dark:bg-[#252529] dark:text-gray-200 md:hidden px-3 py-2 text-sm font-semibold shadow-sm border border-b-gray-200 rounded-full mx-2"
         >
-          <span className="flex">
+          <span className="flex align-middle">
             <span>Menu</span>
             {getDarkModeMenuIcon()}
           </span>
